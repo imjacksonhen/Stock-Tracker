4c6d04266ad32592d785c95ccc844b6540abcf2a
Jackson Chen <imjacksonchen@gmail.com>
2019-05-31 16:44:11 -0400

    Add files via upload

---
+++ src/stockTrack.java
@@ -0,0 +1,22 @@
+/* Stock tracker V. 1
+*  5/31/19
+*/
+
+/* Steps needed to get the price of a stock
+*  1. User input a stock symbol (Ex. JNJ; VOO; DAL) 
+*  2. Program searches for the stock symbol on a website (Google, some stock website)
+*  3. Get the information and represented on the program
+*/
+
+public class stockTrack {
+
+	private static void getValue(){
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
